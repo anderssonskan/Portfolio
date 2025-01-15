a0c2de9bcdbdaed69cc5c9f691e302e31a91cf00
Emma Andersson <anderssons-emma@hotmail.com>
2025-01-15 18:20:54 +0100

    timelineCss

--- src/comps/Timeline.jsx
+++ src/comps/Timeline.jsx
@@ -6,29 +6,41 @@ function Timeline() {
         <div class="timeline">
             <div class="container right">
                 <div class="content">
+                    <h2>Easy Digital Meeting Services AB</h2>
+                    <h3> Frontendutvecklare <br />2024-2025 Uppsala</h3>
+                </div>
+            </div>
+            <div class="container left">
+                <div class="content">
+                    <h2>Frontendutvecklare</h2>
+                    <h3>Jensen yrkeshögskola <br />2023-2025 Kista</h3>
+                </div>
+            </div>
+            <div class="container right">
+                <div class="content">
                     <h2>Pilskolan</h2>
-                    <h3> 2018-2023 Uppsala, Elevassistent</h3>
+                    <h3>Elevassistent <br />2018-2023 Uppsala</h3>
                     <p>Resursskola, låg- och mellanstadiet, med inrikting på barn inom NPF-spektrat.</p>
                 </div>
             </div>
             <div class="container right">
                 <div class="content">
                     <h2>Södersjukhuset</h2>
-                    <h3> 2017-2018 Stockholm, Bemanningsassistent</h3>
-                    <p>Ansvar över bemanningen på akutavdelningen.</p>
+                    <h3>Bemanningsassistent <br /> 2017-2018 Stockholm</h3>
+                    <p>Ansvarat över bemanningen på akutavdelningen.</p>
                 </div>
             </div>
             <div class="container right">
                 <div class="content">
                     <h2>Sveriges television m.m</h2>
-                    <h3> 2015-2017 Stockholm, olika befattningar</h3>
+                    <h3>Olika befattningar <br />2015-2017 Stockholm</h3>
                     <p>Många olika projektanställningar. Bl.a Musikhjälpen, Eurovision Song Contest och Jul med Ernst.</p>
                 </div>
             </div>
             <div class="container left">
                 <div class="content">
-                    <h2>Tvproduktionsspecialist</h2>
-                    <h3>2014-2016 Gamleby folkhögskola</h3>
+                    <h2>Tv-produktionsspecialist</h2>
+                    <h3>Gamleby folkhögskola <br />2014-2016 Gamleby</h3>
                 </div>
             </div>
         </div>
